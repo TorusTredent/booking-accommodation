b355333e2859289827f04eed1f1f9ddab5203a68
TorusTredent <torus.tredent@gmail.com>
2022-02-18 21:38:39 +0300

    delete save method for creat rooms and hotels

--- src/main/java/by/bookingaccommodation/controller/PreviewController.java
+++ src/main/java/by/bookingaccommodation/controller/PreviewController.java
@@ -30,18 +30,8 @@ public class PreviewController {
     @GetMapping()
     public String preview(HttpSession session) {
         session.setAttribute("country", "Belarus");
-        save();
         return "preview";
     }
 
-    private void save() {
-        List<String> imageUrls = new ArrayList<>();
-        imageUrls.add("https://media.wired.com/photos/5fb70f2ce7b75db783b7012c/master/pass/Gear-Photos-597589287.jpg");
-        imageUrls.add("https://cdn.pocket-lint.com/r/s/1200x/assets/images/151442-cameras-feature-stunning-photos-from-the-national-sony-world-photography-awards-2020-image1-evuxphd3mr.jpg");
-        imageUrls.add("https://iso.500px.com/wp-content/uploads/2016/03/stock-photo-142984111.jpg");
-        imageUrls.add("https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg");
-        IntStream.range(0, 20).mapToObj(i -> new Hotel("Name", "Belarus", "Minsk", "Telmana", "53/3", "+375336405535", 3, imageUrls)).forEachOrdered(hotel -> {hotelService.save(hotel);});
-        IntStream.range(1, 21).forEachOrdered(i -> roomService.save(new Room(i, --i)));
-    }
 
 }
